d42f8293942f75a6da18ca6ad0170a49bc97a6a6
Carlos Afonso <elfonsi@gmail.com>
2022-12-02 15:08:41 +0000

    Add image building script

---
+++ image/build_image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ -z $REGISTRY_URL ]; then
+    echo "Can't build image: \$REGISTRY_URL not defined"
+    exit 1
+fi
+
+if [ -z $IMAGE_NAME ]; then
+    echo "Can't build image: \$IMAGE_NAME not defined"
+    exit 1
+fi
+
+SUFFIX=$(date +%Y%m%d%H%M%S)
+docker build . -t "$REGISTRY_URL/$IMAGE_NAME:$SUFFIX"
+docker push "$REGISTRY_URL/$IMAGE_NAME:$SUFFIX"
+
+echo "Image built successfully: $REGISTRY_URL/$IMAGE_NAME:$SUFFIX"
