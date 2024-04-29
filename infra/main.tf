6a9b75f28fb5c94675322f43a9602bc9b9cf58ed
Carlos Afonso <fonsi@google.com>
2024-04-29 12:11:44 +0000

    Bump TF provider version.

--- infra/main.tf
+++ infra/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.16.0"
+      version = "~> 5.26.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.16.0"
+      version = "~> 5.26.0"
     }
   }
 }
